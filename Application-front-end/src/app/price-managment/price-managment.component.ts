8b97f8b710c1f41aa36bf7b5117e92055521e7d1
Tokihery <tokiherynambinintsoatoky@gmail.com>
2021-09-03 00:02:51 +0300

    [ADD][FIX] functionnalité

--- Application-front-end/src/app/price-managment/price-managment.component.ts
+++ Application-front-end/src/app/price-managment/price-managment.component.ts
@@ -5,8 +5,7 @@ import { map, shareReplay, startWith,  switchMap } from 'rxjs/operators';
 import {ResServiceService} from "src/app/restaurant/res-service.service"
 import { Food, Product, Ingredient, FoodsReturn } from "src/app/restaurant/interface"
 import { API} from 'src/environments/environment'
-
-
+import {FormBuilder, FormControl, FormGroup, FormArray, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-price-managment',
@@ -16,14 +15,21 @@ import { API} from 'src/environments/environment'
 export class PriceManagmentComponent implements OnInit {
 
   @ViewChild(MatAccordion) accordion: MatAccordion|any;
+  inputPromCurrent = "0-1"
+  increment =0
   hasCurrentFoodsEdit:boolean = false
   current_foods:Food|any   
   foods:Food[]|any
+  priceForm:FormGroup
   default_image = API.default_image 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private res:ResServiceService) {
+    private res:ResServiceService,
+    private fb:FormBuilder) {
+    this.priceForm = this.fb.group({
+      "price_variante": this.fb.array([]) ,
+      })
     }
 
   ngOnInit(): void {
@@ -73,5 +79,36 @@ export class PriceManagmentComponent implements OnInit {
       console.log(data)
     })
 
+  }
+  get price_variante(){
+    return this.priceForm.controls['price_variante'] as FormArray
+  }
+  updatedPrice(){
+
+  }
+  canceled(){
+
+  }
+  addOtherPrice(){
+    this.increment+=(parseInt(this.inputPromCurrent?.split('-')[1])*1) + 9
+    let promo =this.inputPromCurrent?.split('-')[1] +'-'+this.increment
+    const othersFormPrice = this.fb.group({
+      "price":['', Validators.required],
+      "promotion":[promo, Validators.required],
+      })
+    this.inputPromCurrent = promo
+
+    this.price_variante.push(othersFormPrice)
+    console.log(this.inputPromCurrent)
+  }
+  onChangeProduct($event:any){
+        this.inputPromCurrent = $event
+  }
+  addOther(variante:any){
+    console.log(variante)
+
+  }
+  remove(ingredientsIndex:number){
+    this.price_variante.removeAt(ingredientsIndex)
   }
 }
